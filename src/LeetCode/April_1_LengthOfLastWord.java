fbd78d67d0d7ebbf5ebfab7038cf05c6b1bf146a
anshikabansal <anshikabansal13@gmail.com>
2024-04-01 15:02:52 +0530

    March - Day 1 April POTD for LeetCode and Gfg

---
+++ src/LeetCode/April_1_LengthOfLastWord.java
@@ -0,0 +1,8 @@
+package LeetCode;
+
+public class April_1_LengthOfLastWord {
+    public int lengthOfLastWord(String s) {
+        String[] arr=s.split(" ");
+        return arr[arr.length-1].length();
+    }
+}
